c64868b550df04c75b0b650adbf3508e6ee6384d
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2020-10-02 16:20:20 +0200

    drop zero and other commands

--- R/nice_corrmatrix.R
+++ R/nice_corrmatrix.R
@@ -7,25 +7,29 @@
 #' @param lower TRUE if lower triangle should be included
 #' @param alpha Alpha level
 #' @param digits Round to given digit position
-#' @param n_sig Charcter indexing non-significant values
+#' @param char_nsig Charcter indexing non-significant values
+#' @param char_NA Character for NA values
+#' @param char_autocor Character for diagonal (e.g. "-" or "1.00")
+#' @param char_p10 Character indexing .10 significance level
 #' @param values TRUE if values should be included
 #' @param stars TRUE if stars should be included
-#' @param autocor Character for diagonal (e.g. "-" or "1.00")
-#' @param square Report squared r instead of r
 #' @param caption Caption for a html table.
-#' @param sig.10 Character indexing .10 significance level
+
 #' @param type Charcater string. "df" for data-frame. "html" for html table (needs knitr and kableExtra packages)
 #'
 #' @return A data-frame or a html table object
 #' @export
 
 nice_corrmatrix <- function(cr, upper = TRUE, lower = TRUE,
-                            alpha = .10, digits = 2, n_sig,
-                            values = TRUE, stars = TRUE, autocor = "-",
-                            square = FALSE, caption = "Correlation matrix.",
-                            sig.10 = "\u271d", type = "df",
+                            alpha = .10, digits = 2, 
+                            values = TRUE, stars = TRUE, 
                             numbered_columns = FALSE,
-                            descriptives = TRUE) {
+                            descriptives = TRUE,
+                            char_nsig, char_autocor = "-",
+                            char_p10 = "\u271d", char_NA = "", 
+                            caption = "Correlation matrix.",
+                            drop_zero = TRUE,
+                            type = "df") {
   
   if ("data.frame" %in% class(cr)) {
     .means <- apply(cr, 2, function(x) mean(x, na.rm = TRUE))
@@ -35,33 +39,36 @@ nice_corrmatrix <- function(cr, upper = TRUE, lower = TRUE,
   
   r <- cr$r
   p <- cr$p
-  if (square) r <- r * r
-  r <- format(round(r, digits = digits), nsmall = digits)  #round(r, digits)
-  copt <- r
+
+  r <- format(round(r, digits = digits), nsmall = digits)
+
   if (!values) r[TRUE] <- ""
 
-  if (!missing(n_sig)) r[p > alpha] <- n_sig
-  #copt[copt == 1] <- paste0(autocor, "    ")
-  #copt[copt == 1] <- paste0(autocor, "    ")
-  diag(copt) <- paste0(autocor, "")
+  if (!missing(char_nsig)) r[p > alpha] <- char_nsig
+  r[is.na(cr$r)] <- char_NA
+  diag(r) <- char_autocor
   diag(p) <- 1
   if (stars) {
-    copt[which(p <= .10)] <- paste0(r[which(p <= .10)], sig.10)
-    copt[which(p <= .05)] <- paste0(r[which(p <= .05)], "*")
-    copt[which(p <= .01)] <- paste0(r[which(p <= .01)], "**")
-    copt[which(p <= .001)] <- paste0(r[which(p <= .001)], "***")
-    copt[which(p > .10 & p < 1)] <- paste0(r[which(p > .10 & p < 1)], "")
-    r <- copt
+    copy_r <- r
+    r[which(p <= .10)] <- paste0(copy_r[which(p <= .10)], char_p10)
+    r[which(p <= .05)] <- paste0(copy_r[which(p <= .05)], "*  ")
+    r[which(p <= .01)] <- paste0(copy_r[which(p <= .01)], "** ")
+    r[which(p <= .001)] <- paste0(copy_r[which(p <= .001)], "***")
+    r[which(p > .10 & p < 1)] <- paste0(copy_r[which(p > .10 & p < 1)], "   ")
   }
   
-  if (!upper) r[upper.tri(r)] <- " "
-  if (!lower) r[lower.tri(r)] <- " "
+  r[which(cr$r >= 0)] <- paste0(" ", r[which(cr$r >= 0)])
+  
+  if (!upper) r[upper.tri(r)] <- ""
+  if (!lower) r[lower.tri(r)] <- ""
+  
+  if (drop_zero) r <- gsub("0\\.", ".", r)
   
   r <- as.data.frame(r)
   
   if (numbered_columns) {
-    rownames(r) <- paste0("", 1:nrow(r), " ", rownames(r))
-    colnames(r) <- paste0("", 1:nrow(r), "") 
+    rownames(r) <- paste0(1:nrow(r), " ", rownames(r))
+    colnames(r) <- paste0(1:nrow(r), "") 
   }
   
   if (descriptives) {
@@ -73,8 +80,7 @@ nice_corrmatrix <- function(cr, upper = TRUE, lower = TRUE,
   
   if (type == "df") {
     cat("Correlation matrix.\nProbability signs above the diagonal are adjusted for multiple tests.\n")
-    if (square) cat("Explained variance r-squared\n")
-    if (stars) cat(sig.10, "p<.10; *p<.05; **p<.01; ***p<.001.\n", sep = "")
+    if (stars) cat(char_p10, "p<.10; *p<.05; **p<.01; ***p<.001.\n", sep = "")
     cat("\n")
     r <- format(r, justify = "left")
     return(r)
@@ -85,7 +91,7 @@ nice_corrmatrix <- function(cr, upper = TRUE, lower = TRUE,
       kableExtra::kable_styling(bootstrap_options = "basic", full_width = FALSE) %>%
       kableExtra::column_spec(1, bold = TRUE, color = "black") %>%
       kableExtra::row_spec(1, hline_after = TRUE) %>%
-      kableExtra::footnote(general = paste0(sig.10, "p < .10; *p < .05; **p < .01; ***p < .001")) %>%
+      kableExtra::footnote(general = paste0(char_p10, "p < .10; \\*p < .05; \\*\\*p < .01; \\*\\*\\*p < .001")) %>%
       return()
   }
 }
