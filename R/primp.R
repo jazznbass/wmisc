8343a6fbe2b18734ecd3e80b54e9bb905346c226
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2023-02-13 13:40:43 +0100

    init

--- R/primp.R
+++ R/primp.R
@@ -10,14 +10,16 @@
 #' "bg-lightred", "bg-lightgreen", "bg-lightyellow", "bg-lightblue",
 #' "bg-lightmagenta", "bg-lightcyan", "bg-lightwhite"
 #'
-#' @param x A text
-#' @param ... Style words
-#'
-#' @return
+#' @param x A text containing possible styles in curly brackets, separated by
+#'   comma, semicolon or space.
+#' @param cat TRUE or FALSE
+#' @return Cat a string or returns string
 #' @export
 #'
 #' @examples
-primp <- function(x, ...) {
+#' primp("{green bold}Jo Lady, {blue,italic}that's me, {bg-red}in a tree!")
+#' @export
+primp <- function(x, cat = TRUE) {
   
   styles <- c(black = 30, red = 31, green = 32, yellow = 33, 
               blue = 34, magenta = 35, cyan = 36, white = 37)
@@ -26,14 +28,31 @@ primp <- function(x, ...) {
   styles <- c(styles, bold = 1, faint = 2, italic = 3, underline = 4, swap = 7,
               strike = 9)
   
-  args <- list(...)
+  start <- unlist(gregexpr('\\{', x))
+  stop <- unlist(gregexpr('\\}', x))
+  n_styles <- length(start)
   
-  args <- lapply(args, function(x) {
-    if(is.numeric(x)) x else setNames(styles[which(x == names(styles))], NULL)
+  args <- character(n_styles)
+  for(i in seq_along(start)) {
+    extract <- substr(x, start[i]+1, stop[i]-1)
+    extract <- unlist(strsplit(extract, "[,; ]"))
+    extract <- sapply(extract, function(x) {
+      .cset(setNames(styles[which(trimws(x) == names(styles))], NULL))
+    })
+    args[i] <- paste0(extract, collapse = "")
   }
-  )
-  values <- paste0(args, collapse = ";")
   
-  out <- paste0(cset(values),x,cset())
-  cat(out)
+  out <- c(substr(x, 0, start[1]-1), args[1])
+  for(i in 2:n_styles) {
+    out <- c(out, substr(x, stop[i-1]+1, start[i]-1), .cset(), args[i])
+  }
+  
+  out <- c(out, substr(x, stop[n_styles]+1, nchar(x)), .cset())
+  out <- paste0(out, collapse = "")
+  if (cat) cat(out) else out
+}
+
+.cset <- function(x) {
+  if (missing(x)) x <- "0" 
+  paste0("\033[", x, "m")
 }
