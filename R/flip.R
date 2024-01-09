792f7cf1cb4f275ac34c2f8abfb894decb593102
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2024-01-09 21:22:41 +0100

    character rownames

--- R/flip.R
+++ R/flip.R
@@ -5,13 +5,12 @@
 #' resulting data frame.
 #'
 #' @param x A data frame or matrix to be flipped.
-#' @param rownames Logical. If \code{TRUE}, the resulting data frame will have
-#'   row names (if \code{x} is a matrix) or row names will be included as the 
-#'   first column (if \code{x} is a data frame).
+#' @param rownames Logical or string. If `TRUE`, row names will be included as the 
+#'   first column. If `character`, the column will be named respectively.
 #' 
 #' @return A data frame with flipped rows and columns. If \code{rownames} is 
-#'   \code{TRUE}, row names (if \code{x} is a matrix) or the first column will 
-#'   contain the original row names (if \code{x} is a data frame).
+#'   \code{TRUE}, row names (if `x` is a matrix) or the first column will 
+#'   contain the original row names (if `x` is a data frame).
 #' 
 #' @export
 #' @examples
@@ -23,19 +22,15 @@
 #' 
 #' # Flip the data frame with row names included
 #' flipped_df_with_rownames <- flip(df, rownames = TRUE)
-#' 
-#' # Create a sample matrix
-#' mat <- matrix(1:9, nrow = 3)
-#' 
-#' # Flip the matrix
-#' flipped_mat <- flip(mat)
-#' 
-#' # Flip the matrix with row names included
-#' flipped_mat_with_rownames <- flip(mat, rownames = TRUE)
 #'
 #' @seealso \code{\link{t}} for transposing matrices and data frames.
-flip <- function(x, rownames = FALSE) {
-  if (rownames) x <- cbind(" " = rownames(x), x)
+flip <- function(x, rownames = NULL) {
+  if (isTRUE(rownames)) x <- cbind(" " = rownames(x), x)
+  if (is.character(rownames)) {
+    rn <- data.frame(x = rownames(x))
+    names(rn) <- rownames
+    x <- cbind(rn, x)
+  }
   x <- as.data.frame(t(x))
   x <- setNames(x, x[1, ]) 
   x <- cbind(" " = rownames(x), x)
