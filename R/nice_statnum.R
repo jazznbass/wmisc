52d441d61dd8fa45d0b2980e08c70e2476685515
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2024-02-05 16:01:20 +0100

    upd

--- R/nice_statnum.R
+++ R/nice_statnum.R
@@ -3,6 +3,9 @@
 #' 
 #' @param x number
 #' @param digits Number to round to
+#' @examples
+#' nice_statnum(c(1, -1, runif(4, -1, 1)))
+#' 
 #' @export
 nice_statnum <- function(x, digits = 2) {
   sub("^(-?)0.", "\\1.", sprintf(paste0("%.", digits, "f"), x))
