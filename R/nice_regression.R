cd4e74dfa934ee289695d063d6d22c7331c95860
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2024-02-08 13:54:45 +0100

    init

---
+++ R/nice_regression.R
@@ -0,0 +1,140 @@
+
+#' @export
+nice_regression_table <- function(
+    ..., 
+    digits = 2, 
+    label_models = NULL,
+    label_predictor = "Predictor",
+    label_estimate = "Estimate",
+    label_se = "SE",
+    label_statistic = "t",
+    label_p = "p",
+    rename_labels = list()
+) {
+  
+  models <- list(...)
+  
+  cl <- class(models[[1]])
+  n_models <- length(models)
+  
+  #if (inherits(models[[1]], "lm"))
+  models_summary <- lapply(models, summary)
+  #if (inherits(models[[1]], "lme"))
+  #  models_summary <- models
+  
+  
+  get_coef_table <- function(x) {
+    out <- as.data.frame(coef(x))
+    
+    rn <- list(
+      Value = label_estimate,
+      Estimate = label_estimate,
+      "t-value" = "t",
+      "t value" = "t",
+      "Std.Error" = label_se,
+      "Std. Error" = label_se,
+      "Pr(>|t|)" = label_p,
+      "t-value" = "t"      
+    )
+    names(out) <- change_by_list(names(out), rn)
+    out$p <- nice_p(out$p, stars = TRUE)
+    out
+  }
+  
+  out <- lapply(models_summary, \(x) get_coef_table(x))
+  n_param <- ncol(out[[1]])
+  
+  if (length(out) > 1) out <- do.call(combine_cols, out) else out <- out[[1]]
+
+  names(out) <- make.unique(names(out))
+  
+  cols_label <- as.list(rep(names(out)[1:n_param], length(models)))
+  names(cols_label) <- names(out)
+
+  out <- cbind(var = rownames(out), out)
+  rownames(out) <- NULL
+  
+  n_predic <- nrow(out)
+  
+  add_param <- function(param, label) {
+    out[nrow(out) + 1, 1] <- label
+    out[nrow(out), 2 + (0:(n_models - 1)) * n_param] <- param
+    out
+  }
+  
+  if (cl == "lm") n <- lapply(models, \(x) nrow(x$model)) |> unlist()
+  if (cl == "lme") n <- lapply(models, \(x) nrow(x$data)) |> unlist()
+  if (cl == "lmerModLmerTest") n <- lapply(models, \(x) length(x@resp$y)) |> unlist()
+  
+  out <- add_param(n, "N")
+
+  if (cl == "lm") {
+    out <- add_param(
+      lapply(models_summary, \(x) x$r.squared) |> unlist(), 
+      "R\u00b2"
+    )
+    out <- add_param(
+      lapply(models_summary, \(x) x$adj.r.squared) |> unlist(), 
+      "R\u00b2 adjusted"
+    )
+  }
+
+  out <- round_numeric(out, digit = digits)
+  
+  names(out)[1] <- label_predictor
+  
+  if (cl == "lmerModLmerTest") {
+    auto_labels <- lapply(models, \(x) get_labels(x@frame)) |> unlist()
+  }
+
+  if (cl %in% c("lm", "lme")) {
+    auto_labels <- lapply(models, \(x) get_labels(x$model)) |> unlist()
+  }
+  
+  auto_labels <- c(unlist(rename_labels), auto_labels)
+  
+  out[[1]] <- change_by_list(out[[1]], auto_labels)
+  
+  # spanner ----
+  
+  spanner <- 2 + (0:(n_models - 1)) * n_param
+  spanner <- lapply(spanner, \(x) x:(x + n_param - 1))
+  
+  if (is.null(label_models)) {
+    if (cl == "lmerModLmerTest") {
+      label_models <- unlist(
+        lapply(models, \(x) x@call$formula[[2]] |> as.character())
+      )
+    } else {
+      label_models <- unlist(
+        lapply(models, \(x) x$terms[[2]] |> as.character())
+      )
+    }
+
+    label_models <- change_by_list(label_models, auto_labels)
+    label_models <- make.unique(label_models, sep = " ")
+  }
+  
+  names(spanner) <- label_models
+  
+  row_group <- list(
+    #Coefficients = 1:(n_predic),
+    Model = (n_predic + 1):nrow(out)
+  )
+  
+  # out -----
+  
+  out <- set_wmisc_attributes(out,
+    cols_label = cols_label,
+    spanner = spanner,
+    row_group = row_group,
+    label_na = "",
+    title = "Regression model"
+  )
+  nice_table(out) |> 
+    gt::row_group_order(groups = c(NA, "Model"))
+  
+}
+
+
+
