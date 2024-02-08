f548416e4ae821efe9db05a3b1086d13915890ab
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2024-02-08 13:54:39 +0100

    new arg stars

--- R/nice_p.R
+++ R/nice_p.R
@@ -9,19 +9,41 @@
 #' p <- c(0.04, 0.9, 0.10, 0.001, 1, NA)
 #' nice_p(p)
 #' paste0("p", nice_p(p, equal_sign = TRUE))
-nice_p <- function(p, equal_sign = FALSE, digits = 3) {
-  unlist(lapply(p, .nice_p, equal_sign = equal_sign, digits = digits))
+nice_p <- function(p, equal_sign = FALSE, digits = 3, stars = FALSE) {
+  unlist(
+    lapply(p, .nice_p, equal_sign = equal_sign, digits = digits, stars = stars)
+  )
 }
 
-.nice_p <- function(p, equal_sign, digits) {
+.nice_p <- function(p, equal_sign, digits, stars) {
   if (is.na(p)) return(NA)
   equal_sign <- if (equal_sign) "=" else ""
   if (isTRUE(p >= 0.05)) {
     return(paste0(equal_sign, nice_statnum(p, digits = digits)))
   }
-  if (isTRUE(p < 0.001)) return("<.001")
-  if (isTRUE(p < 0.01)) return("<.01")
-  if (isTRUE(p < 0.05)) return("<.05")
+  if (isTRUE(p < 0.001)) {
+    if (!stars) {
+      return("<.001") 
+    } else {
+      return("<.001***") 
+      #return(paste0(equal_sign, nice_statnum(p, digits = digits), "***"))
+    }
+
+  }
+  if (isTRUE(p < 0.01)) {
+    if (!stars) {
+      return("<.01") 
+    } else {
+      return(paste0(equal_sign, nice_statnum(p, digits = digits), "**"))
+    }
+  }
+  if (isTRUE(p < 0.05)) {
+    if (!stars) {
+      return("<.05") 
+    } else {
+      return(paste0(equal_sign, nice_statnum(p, digits = digits), "*"))
+    }
+  }
 }
 
 
