5a044afcc146b31037a4255139e976daa30a97e6
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2024-03-07 09:38:08 +0100

    upd

--- R/nice_sem.R
+++ R/nice_sem.R
@@ -2,25 +2,34 @@
 #' 
 #' @param x An object returned from the lavaan sem or cfa function.
 #' @param remove_cols Either column number or column names to be removed. 
-#' @param title Title string.
-#' @param footnote/note Add footnote
+#' @param show_fitmeasures A named vector with fit measures.
+#' @param show_ci If TRUE, adds columns with 95% confidence intervals.
+#' @param ... Further arguments passed to the `nice_table()` function.
 #' @examples
 #' nice_sem(wmisc::lavaan_fit)
 #' @export
 nice_sem <- function(x, 
                      standardized = TRUE, 
-                     fitmeasures = c(
+                     show_fitmeasures = c(
                        CFI = "cfi", TLI = "tli", RMSEA = "rmsea", 
                        "RMSEA ci lower" = "rmsea.ci.lower",
+                       "SRMR" = "srmr", 
                        AIC = "aic", BIC = "bic"),
                      remove_cols = NULL,
+                     show_ci = TRUE,
                      round = 2,
-                     title = "Structure equation model",
-                     footnote = NULL,
-                     file = NULL) {
+                     ...) {
 
-  if (standardized) out <- lavaan::standardizedsolution(x) |> as.data.frame()
-  if (!standardized) out <- lavaan::parameterestimates(x) |> as.data.frame()
+  
+  args_nice_table <- list(...)
+  
+  if (standardized) {
+    out <- lavaan::standardizedsolution(x) |> as.data.frame()
+    char_estimate <- "ß"
+  } else {
+    out <- lavaan::parameterestimates(x) |> as.data.frame()
+    char_estimate <- "b"
+  }
   
   rows_latents <- which(out$op == "=~")
   rows_regressions <- which(out$op == "~")
@@ -43,29 +52,25 @@ nice_sem <- function(x,
   row_group <- row_group[sapply(row_group, \(.) length(.) > 0)]
   
   out <- out[, -c(2:3)]
-  names(out) <- c("Parameter", "b", "se", "z", "p", "lower", "upper")
+  names(out) <- c("parameter", char_estimate, "se", "z", "p", "lower", "upper")
   out$p <- nice_p(out$p)
-  out <- out[,c("Parameter", "b", "lower", "upper", "se", "z", "p")]
+  out <- out[,c("parameter", char_estimate, "lower", "upper", "se", "z", "p")]
   
   # remove cols ----
   
-  remove_ci <- FALSE
+  if (!show_ci) out <- out[,-(3:4)]
   if (!is.null(remove_cols)) {
     if (is.character(remove_cols)) {
-      if ("ci" %in% remove_cols) remove_ci <- TRUE
       remove_cols <- which(names(out) %in% remove_cols)
-      if (remove_ci) remove_cols <- c(3:4, remove_cols)
     }
     out <- out[,-remove_cols]
   }
   
-  
-  
   # add fit measures ----
   
-  fit_first <- fitmeasures(x)
+  fit_first <- lavaan::fitmeasures(x)
   
-  out[nrow(out)+1, 1] <- "Parameters"
+  out[nrow(out)+1, 1] <- "N parameters"
   out[nrow(out), 2] <- fit_first["npar"]
   out[nrow(out)+1, 1] <- "Observations"
   out[nrow(out), 2] <- fit_first["ntotal"]
@@ -76,10 +81,10 @@ nice_sem <- function(x,
   #out[nrow(out)+1, 1] <- "p"
   out[nrow(out), 3] <- fit_first["pvalue"]
   
-  fit <- fitmeasures(x, fitmeasures)
+  fit <- lavaan::fitmeasures(x, show_fitmeasures)
   
   rows <- (nrow(out)+1):(nrow(out)+length(fit))
-  out[rows, 1] <- names(fitmeasures)
+  out[rows, 1] <- names(show_fitmeasures)
   out[rows, 2] <- fit
 
   row_group$"Modelfit" <- (nrow(out) - length(fit) - 3):nrow(out)
@@ -91,20 +96,40 @@ nice_sem <- function(x,
     if(!standardized) footnote <- "Effect estimates are not standardized"
   }
   
+  if (is.null(args_nice_table$title)) args_nice_table$title <- "Structure equation model"
+
+  if (is.null(args_nice_table$footnote)) {
+    cit <- unclass(citation("lavaan"))[[1]]
+    args_nice_table$footnote <- c(
+      
+      if (x@Options$estimator == "ML") {
+        "The estimation method employed in this analysis is Maximum Likelihood (ML)"
+      } else paste0("The estimation method employed in this analysis is ", x@Options$estimator),  
+      if (x@Options$optim.method == "nlminb") {
+       "The Non-Linear Minimization, Bounded (nlminb) algorithm was applied for optimization"
+      } else paste0("Optimization method is ", x@Options$optim.method),
+      if (identical(x@Options$missing, "ml")) 
+        "Missing data were addressed using the Full Information Maximum Likelihood (FIML) approach",
+      paste0("The analysis was performed with the lavaan package in R (",
+              cit$author, ", ", cit$year, ")")
+      
+      
+    )
+  }
+  
   out <- set_wmisc_attributes(
     out,
-    title = title,
     row_group = row_group,
-    footnote = footnote,
-    label_na = "",
-    file = file
+    label_na = ""
   )
   
-  if (!remove_ci) out <- set_wmisc_attributes(
-    out,spanner = list("95% CI" = 3:4))
-
-  nice_table(out)
+  if (show_ci) out <- set_wmisc_attributes(
+    out, spanner = list("95% CI" = 3:4))
   
+  args_nice_table$x <- out
+
+  do.call(nice_table, args_nice_table)
+
 }
 
 
