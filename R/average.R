b8506929860fc8bbd803a99bb3a9a6599091faa4
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2024-08-15 10:00:54 +0200

    generalization of average

--- R/average.R
+++ R/average.R
@@ -9,6 +9,8 @@
 #' @param max_na Maximum number of NAs that are allowed before returning NA. A
 #'   value between 0 and 1 indicates a proportion of values (e.g., 0.5 = 50
 #'   percent NAs are allowed).
+#' @param func Function without the header `function(.)` for calculating the
+#'   average.
 #' @export
 #' @examples
 #' dat <- c(1:5, NA, NA)
@@ -16,11 +18,15 @@
 #' average(dat, min_valid = 6)
 #' average(dat, max_na = 2)
 #' average(dat, max_na = 1)
-#' average(dat, min_valid = 0.5)
+#' average(dat, min_valid = 0.5, func = median(.))
 #' average(dat, min_valid = 0.9)
 
-average <- function(x, min_valid, max_na) {
+average <- function(x, min_valid, max_na, func = mean(.)) {
   
+  envi <- new.env()
+  
+  func <- deparse(substitute(func))
+ 
   if (!missing(min_valid)) {
     if (min_valid < 1 && min_valid > 0) min_valid <- trunc(min_valid * length(x))
     if(sum(!is.na(x)) < min_valid) {
@@ -34,20 +40,6 @@ average <- function(x, min_valid, max_na) {
       return(NA)
     }
   }
-  
-  mean(x, na.rm = TRUE)
-  
+  envi$. <- x[which(!is.na(x))]
+  eval(str2lang(func), envir = envi)
 }
-
-
-### generalization of mean.
-.FUN <- function(x, fun = mean, min = length(x)) {
-  if (min < 1 && min > 0) min <- trunc(min * length(x))
-  if (min < 0) min <- length(x) + min
-  if (sum(!is.na(x)) >= min) {
-    return(fun(x[which(!is.na(x))]))
-  } else {
-    return(NA)
-  }
-}
-
