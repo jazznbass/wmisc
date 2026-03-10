370ee8ff56a3ebed7360e8e843ba2b2d701272eb
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2026-03-10 19:17:04 +0100

    cleared bug

--- R/nice_frequencies.R
+++ R/nice_frequencies.R
@@ -1,15 +1,32 @@
 #' Table with frequency statistics
 #'
+#' This function creates a frequency table for a given variable, optionally
+#' grouped by another variable. It can include counts, percentages, and totals,
+#' with customizable labels and formatting options.
+#'
+#' @details
+#' The resulting data frame contains the frequency counts and, if specified, the
+#' percentages for each level of the variable and grouping variable. The table can
+#' also include totals for rows and columns, and a row for missing values if desired.
+#' The resulting data frame is decorated with attributes for use with the
+#' \code{wmisc::nice_table()} function, which is called internally to create the final
+#' output.
+#'
 #' @param data Vector.
 #' @param grouping A grouping variable as a vector.
 #' @param label Set label for the variable name.
 #' @param label_grouping Set label for the grouping variable name.
 #' @param show_missing If TRUE, adds a row for the number of missing values.
 #' @param show_percent If TRUE, adds a column for percentages.
-#' @param percent_base If show_percent is TRUE, this argument specifies the base for percentage calculations.
-#'  Options are "column" (percentages calculated within each column), "row"
-#'  (percentages calculated within each row), or "total" (percentages calculated based on the total count).
-#'  Default is "column".
+#' @param percent_base If show_percent is TRUE, this argument specifies the base
+#'   for percentage calculations. Options are "column" (percentages calculated
+#'   within each column), "row" (percentages calculated within each row), or
+#'   "total" (percentages calculated based on the total count). Default is
+#'   "column".
+#' @param show_total_col If TRUE, adds a column with the total counts and
+#'   percentages (if show_percent is TRUE).
+#' @param show_total_row If TRUE, adds a row with the total counts and
+#'   percentages (if show_percent is TRUE).
 #' @param auto_labels If TRUE, variable names are taken from a label attribute.
 #' @param title Table title.
 #' @param footnote Table footnote.
@@ -17,20 +34,26 @@
 #' @param ... Further arguments passed to [nice_table()].
 #' @return An html table with frequencies
 #' @examples
-#' nice_frequencies(wmisc:::mtcars_labeled[[11]])
+#' nice_frequencies(mtcars_labeled[[11]])
+#'
+#' ## cross table
 #' nice_frequencies(
-#'   wmisc:::mtcars_labeled$cyl, 
-#'   wmisc:::mtcars_labeled$am
+#'   mtcars_labeled$cyl,
+#'   mtcars_labeled$am
 #' )
+#'
+#' ## barebone table
 #' nice_frequencies(
-#'  wmisc:::mtcars_labeled$cyl,
-#'  wmisc:::mtcars_labeled$am,
+#'  mtcars_labeled$cyl,
+#'  mtcars_labeled$am,
 #'  show_missing = FALSE,
-#'  show_percent = FALSE
+#'  show_percent = FALSE,
+#'  show_total_col = FALSE,
+#'  show_total_row = FALSE
 #' )
 #' nice_frequencies(
-#'   wmisc:::mtcars_labeled$cyl,
-#'   wmisc:::mtcars_labeled$am,
+#'   mtcars_labeled$cyl,
+#'   mtcars_labeled$am,
 #'   label = "Cylinders",
 #'   label_grouping = "Transmission",
 #'   title = "Cylinders by Transmission Type"
@@ -43,7 +66,8 @@ nice_frequencies <- function(data,
                              show_missing = TRUE,
                              show_percent = TRUE,
                              percent_base = "column",
-                             add_total = TRUE, 
+                             show_total_col = TRUE, 
+                             show_total_row = TRUE,
                              auto_labels = TRUE,
                              title = NULL,
                              footnote = NULL,
@@ -51,10 +75,10 @@ nice_frequencies <- function(data,
                              round = 1,
                              ...) {
   
-  init_messages(); on.exit(print_messages())
+  ## init_messages(); on.exit(print_messages())
   
   if (show_percent && !percent_base %in% c("column", "row", "total", "cell")) {
-    add_message(
+    notify(
       "Invalid value for 'percent_base'. Defaulting to 'column'."
     )
     percent_base <- "column"
@@ -87,9 +111,9 @@ nice_frequencies <- function(data,
     out <- as.data.frame(tab)
     names(out)[1] <- "Frequency"
     if (show_percent) {
-      out$Percent = round(out[[1]]/sum(out[[1]], na.rm = TRUE)*100)
+      out$Percent = out[[1]] / sum(out[[1]], na.rm = TRUE) * 100
     }
-    if (add_total) {
+    if (show_total_col) {
       .total <- sum(out[[1]], na.rm = TRUE)
       if (show_percent) {
         .total[2] <- sum(out$Percent, na.rm = TRUE)
@@ -99,14 +123,14 @@ nice_frequencies <- function(data,
     
     spanner <- NULL
     rn <- rownames(out)
-  } else {
+  } else {          ## is.null(grouping)
     tab <- table(data, grouping, useNA = useNA)
     class(tab) <- NULL
     out <- as.data.frame(tab)
     out[ncol(out)] <- NULL
     rn <- row.names(tab)
     group_levels <- ncol(out)
-  
+    
     if (show_percent) {
       if (percent_base == "column") {
         perc <- proportions(as.matrix(out) , margin = 2)
@@ -119,11 +143,25 @@ nice_frequencies <- function(data,
       } 
       perc <- as.data.frame(perc * 100)
       names(perc) <- paste0(" ", names(out), " ")
-      if (add_total) {
-        .total <- colSums(out, na.rm = TRUE)
+      
+      if (show_total_row) {
+        .total <- rowSums(out, na.rm = FALSE)
+        out <- cbind(out, Total = .total)
+        if (percent_base == "row") {
+          .total <- rowSums(perc, na.rm = FALSE)
+        } else {
+          .total <- rep(NA, nrow(perc))
+        } 
+       
+        perc <- cbind(perc, " Total " = .total)
+        group_levels <- group_levels + 1
+      }
+      if (show_total_col) {
+        .total <- colSums(out, na.rm = FALSE)
         out <- rbind(out, Total = .total)
+        
         if (percent_base == "column") {
-          .total <- colSums(perc, na.rm = TRUE)
+          .total <- colSums(perc, na.rm = FALSE)
         } else {
           .total <- rep(NA, ncol(perc))
         } 
@@ -134,7 +172,7 @@ nice_frequencies <- function(data,
       out <- cbind(out, perc)
     }
     
-    if (!show_percent && add_total) {
+    if (!show_percent && show_total_col) {
       .total <- colSums(out, na.rm = TRUE)
       out <- rbind(out, Total = .total)
       rn <- c(rn, "Total")
